003d726e96f9704c4947ba8e1eb3341ab4e3e799
Айдын <@aidyn651>
2025-12-21 20:51:04 +0500

    init commit

--- src/com/aidyn/grocerystore/GroceryStore.java
+++ src/com/aidyn/grocerystore/GroceryStore.java
@@ -1,6 +1,6 @@
 package com.aidyn.grocerystore;
 
-public class Main {
+class GroceryStore {
     public static void main(String[] args) {
 
         System.out.println("=== Grocery Store Management System ===");
--- src/com/aidyn/grocerystore/Main.java
+++ src/com/aidyn/grocerystore/Main.java
@@ -1,7 +1,7 @@
 package com.aidyn.grocerystore;
 
-public class Main {
-    public static void main(String[] args) {
+ class Main {
+    static void main(String[] args) {
 
         System.out.println("=== Grocery Store Management System ===");
 
