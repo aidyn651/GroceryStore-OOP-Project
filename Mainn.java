134c95e33cd8fb98da53e5f77c5b8a4dd5b8b9dc
Айдын <@aidyn651>
2026-01-18 12:28:39 +0500

    init commit

---
+++ Mainn.java
@@ -0,0 +1,9 @@
+import model.Menu;
+import model.StoreMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new StoreMenu();
+        menu.run();
+    }
+}
--- modell/StoreMenu.java
+++ modell/StoreMenu.java
@@ -96,4 +96,3 @@ public class StoreMenu implements Menu {
         }
     }
 }
-
